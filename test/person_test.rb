be47f4b24ca570d8a7bead02c229086342a55e51
Johnny Bowman <JohnnyBowman@gmail.com>
2013-04-03 16:32:15 -0400

    made some initial changes to create database

--- test/person_test.rb
+++ test/person_test.rb
@@ -5,17 +5,19 @@ require 'json'
 require 'pry'
 require_relative '../movies'
 
-class MovieGrabberTest < Test::Unit::TestCase
+class MovieGrabberTest < Test:Unit::TestCase
 
   def setup
-    # @@db = SQLite3::Database.new("test/test.db")
+    @@db = SQLite3::Database.new("test/test.db")
   end
 
   def test_get_film_info_method
     movie = Movie.get_film_info("jaws")
-    assert_equal "Jaws", movie.title
-    # Don't forget to create your movies table first, using sqlite3...
-    # movies = @@db.execute("select * from movies")
+    assert_equal "Jaws" varchar, movie.title
+    # created movies table with
+    # create table movies (title varchar, year integer, rating varchar, genre varchar, director varchar, actors varchar, plot varchar, poster_url varchar);
+    
+    movies = @@db.execute("select * from movies")
     # assert_equal 1, movies.length
     # assert_equal "Jaws", movies.first[1] # Note, this can change if you want/need
     # # Add other parts to your test here...
